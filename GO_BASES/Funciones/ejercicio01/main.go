85bf2400059050ee4e5949f19c08dbbff063a131
lucianarivarossa1 <luciana.rivarossa@mercadolibre.com>
2023-09-01 13:06:59 -0300

    agrego ejercicio funciones

---
+++ GO_BASES/Funciones/ejercicio01/main.go
@@ -0,0 +1,24 @@
+package main
+
+const (
+	mas50  = 0.17
+	mas150 = 0.27
+)
+
+func calcularImpuesto(salario float64) float64 {
+	var impuesto float64
+	if salario > 50000 {
+		impuesto = salario*mas50 + salario
+	} else if salario > 150000 {
+		impuesto = salario*mas150 + salario
+	} else {
+		impuesto = salario
+	}
+
+	return impuesto
+
+}
+func main() {
+
+	println(calcularImpuesto(150000.0))
+}
